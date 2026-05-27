0c5868d9b72bade00996b05e99724f2e23608486
Dev Agent <dev@medchain.com>
2026-05-27 09:24:19 +0530

    feat: add patient record endpoints with clinical timeline filters

--- records/urls.py
+++ records/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RecordUploadView, RecordTimelineView
+from .views import RecordUploadView, RecordTimelineView, DoctorPatientRecordsView
 
 urlpatterns = [
     path('', RecordTimelineView.as_view(), name='record_timeline'),
     path('upload/', RecordUploadView.as_view(), name='record_upload'),
+    path('patient/<uuid:patient_id>/', DoctorPatientRecordsView.as_view(), name='doctor_patient_records'),
 ]
